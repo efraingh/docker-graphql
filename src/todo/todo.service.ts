a1e897d15e4608edb75beb3547449bf15093d9cf
Efrain LLC9 <efrainllc9@gmail.com>
2025-04-22 15:06:15 -0400

    major: Cambio total prueba

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -12,7 +12,8 @@ export class TodoService {
         { id: 3, description: 'Piedra del Poder', done: false },
         { id: 4, description: 'Piedra del Tiempo', done: false },
         { id: 5, description: 'Piedra desde el contenedor', done: true },
-        { id: 5, description: 'Piedra desde la computadora', done: true },
+        { id: 6, description: 'Piedra desde la computadora', done: true },
+        { id: 7, description: 'Piedra desde la computadora otro mas', done: true },
     ];
 
     get totalTodos() {
